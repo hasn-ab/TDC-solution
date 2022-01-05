39eb59d18ea91016c98ecb5f39b547f750fc528b
hasn-ab <67307736+hasn-ab@users.noreply.github.com>
2022-01-05 23:18:45 +0500

    feat: added subscription logic in CarMap

--- carmapclient/components/CarMap.tsx
+++ carmapclient/components/CarMap.tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useRef} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {gql, useSubscription} from '@apollo/client';
+import React, {useCallback, useState, useEffect, useRef} from 'react';
+import {Image, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 import SlidingUpPanel from 'rn-sliding-up-panel';
+import CarDetails from './CarDetails';
 import CloseButton from './CloseButton';
 
 type Car = {
@@ -12,17 +14,71 @@ type Car = {
   distanceToDestination: number;
 };
 
+//subscription for getting updated cars
+const CARS_SUBSCRIPTION = gql`
+  subscription CarsUpdated {
+    updatedCars {
+      id
+      location {
+        latitude
+        longitude
+      }
+      destination {
+        latitude
+        longitude
+      }
+      distanceToDestination
+    }
+  }
+`;
+
 const CarMap = () => {
   // At the top of this component you should subscribe to updates from the server.
   // See https://www.apollographql.com/docs/react/data/subscriptions/#executing-a-subscription
-
+  const mapRef = useRef<MapView>();
+  // flag to check if map centered after initial fetch
+  const mapCenteredRef = useRef<Boolean>(false);
+  // subscription for graphl data
+  const {data, loading, error} = useSubscription(CARS_SUBSCRIPTION);
   const panelEl = useRef<SlidingUpPanel>(null);
-
-  useEffect(() => panelEl.current?.show());
+  //put cars data in types cars array
+  const cars: Array<Car> = data?.updatedCars || [];
+  //state for storing tapped car to be displayed in bottomsheet/panel
+  const [selectedCar, setSelectedCar] = useState<Car | null>(null);
+  // effect for setting the viewport to have our current cars visibile
+  useEffect(() => {
+    if (cars.length > 0 && !mapCenteredRef.current) {
+      mapRef.current?.fitToCoordinates(cars.map(car => car.location));
+      mapCenteredRef.current = true;
+    }
+  }, [data?.updatedCars]);
+  //hiding sliding panel initially
+  useEffect(() => panelEl.current?.hide(), []);
+  //call when a car on map is press
+  const onCarTapped = useCallback((car: Car) => {
+    setSelectedCar(car);
+    panelEl.current?.show();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        onLayout={() => {
+          if (cars.length > 0) {
+            mapRef.current?.fitToCoordinates();
+            mapCenteredRef.current = true;
+          }
+        }}>
+        {cars.map(car => (
+          <Marker key={car.id} coordinate={car.location}>
+            <TouchableOpacity onPress={() => onCarTapped(car)}>
+              <Image source={require('./Car.png')} style={styles.markerImage} />
+            </TouchableOpacity>
+          </Marker>
+        ))}
+      </MapView>
       {/* For documentation on the MapView see https://github.com/react-native-maps/react-native-maps */}
       <SlidingUpPanel
         ref={panelEl}
@@ -30,11 +86,16 @@ const CarMap = () => {
         showBackdrop={false}
         allowDragging={false}>
         <View style={styles.panel}>
-          <CloseButton
-            onPress={() => {
-              panelEl.current?.hide();
-            }}
-          />
+          {selectedCar && (
+            <>
+              <CloseButton
+                onPress={() => {
+                  panelEl.current?.hide();
+                }}
+              />
+              <CarDetails car={selectedCar} />
+            </>
+          )}
         </View>
       </SlidingUpPanel>
     </View>
@@ -58,6 +119,22 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingLeft: 20,
     paddingTop: 20,
+  },
+  markerImage: {
+    width: 40,
+    height: 40,
+  },
+  carDetails: {
+    paddingVertical: 18,
+    paddingTop: 18,
+  },
+  carName: {
+    fontSize: 20,
+    fontWeight: '800',
+  },
+  carLocationDetails: {
+    fontSize: 15,
+    fontWeight: '400',
   },
 });
 
